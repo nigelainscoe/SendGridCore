13ae10715de146ce40221060784d20f3686532ac
Nige <nigel@pw6s.com>
2016-07-12 10:43:26 +0100

    Added Swagger

--- src/APITest/Startup.cs
+++ src/APITest/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SendGridCore.Services;
+using Swashbuckle.Swagger.Model;
 
 namespace SendGridCore
 {
@@ -41,6 +42,17 @@ namespace SendGridCore
 
             services.AddSingleton(provider => Configuration);
             services.AddSingleton<IAuthService, AuthService>();
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v0.95",
+                    Title = "Sendgrid API Wrapper",
+                    Description = "Allows a website to make a signup request to a SendGrid marketing list",
+                    TermsOfService = "MIT Licence."
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
@@ -52,6 +64,9 @@ namespace SendGridCore
             app.UseApplicationInsightsRequestTelemetry();
 
             app.UseApplicationInsightsExceptionTelemetry();
+
+            app.UseSwagger();
+            app.UseSwaggerUi();
 
             app.UseMvc();
         }
